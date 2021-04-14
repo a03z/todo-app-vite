4ef79013f0a61c9e764fb460e497dec2589303c8
Daniil <a03zcode@yandex.ru>
2021-04-14 18:14:10 +0300

    Update Todo.tsx

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -12,14 +12,12 @@ interface TodoI {
 export const Todo = () => {
     const todos = useSelector((state: RootStateOrAny) => state.main.todos)
 
-    let Todos = todos.map((todo: TodoI) => {
-        return <TodoItem key={todo.id} message={todo.message} />
-    })
-
     return (
         <Container>
             <AddTodo />
-            {Todos}
+            {todos.map((todo: TodoI) => {
+                return <TodoItem key={todo.id} message={todo.message} />
+            })}
         </Container>
     )
 }
